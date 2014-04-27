b978f8d227c863da63216328fcad4ff850ca23c8
darul <darul75@gmail.com>
2014-04-27 20:20:19 +0200

    new karma version

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,16 +1,22 @@
-basePath = '..';
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'bower_components/jquery/dist/jquery.js',
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'bower_components/tinymce/tinymce.min.js',
-  'bower_components/angular-ui-tinymce/src/tinymce.js',
-  'src/tinymce-typeahead-factory.js',
-  'src/tinymce-typeahead-service.js',
-  'src/tinymce-typeahead.js',
-  'test/*.spec.js'
-];
-singleRun = true;
-browsers = [ 'Chrome' ];
+
+
+ module.exports = function(config) {
+    config.set({
+      basePath : '..',            
+      frameworks: ['jasmine'],
+      files: [        
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/tinymce/tinymce.min.js',
+        'bower_components/angular-ui-tinymce/src/tinymce.js',
+        'src/tinymce-typeahead-factory.js',
+        'src/tinymce-typeahead-service.js',
+        'src/tinymce-typeahead.js',
+        'test/*.spec.js'
+      ],
+      singleRun : true,
+      browsers = [ 'Chrome' ]
+    });
+
+  };
